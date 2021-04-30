70a9d9d7bf41be9fa5e58d40fba216d9b6df008d
Edgars Košovojs <kosovojs@gmail.com>
2021-04-30 22:47:10 +0300

    build

---
+++ frontend/build/precache-manifest.aa54c631e2daa960e2ed37990be8907d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6c5048bbc48a2b8c961dd1b871f0ac",
+    "url": "/booster/index.html"
+  },
+  {
+    "revision": "47afe54c64cd6175c8dd",
+    "url": "/booster/static/css/2.5d00fa23.chunk.css"
+  },
+  {
+    "revision": "629c034bee0d3bd757c1",
+    "url": "/booster/static/css/main.fca3b1d7.chunk.css"
+  },
+  {
+    "revision": "47afe54c64cd6175c8dd",
+    "url": "/booster/static/js/2.8960a752.chunk.js"
+  },
+  {
+    "revision": "629c034bee0d3bd757c1",
+    "url": "/booster/static/js/main.5512f18a.chunk.js"
+  },
+  {
+    "revision": "8f2607ea9acb697b08cf",
+    "url": "/booster/static/js/runtime~main.9b0b7339.js"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/booster/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/booster/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/booster/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/booster/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/booster/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/booster/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/booster/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/booster/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/booster/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/booster/static/media/fa-solid-900.e8a427e1.woff2"
+  }
+]);
